656042feeff3359100e74264b6f5c81887b3f2a8
Elizabeth Tukaeva <elizabeth77531@gmail.com>
2020-09-28 18:58:20 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+exports.min = function min(array) {
+    if (!Array.isArray(array) || array.length == 0) {
+        return 0;
+    } else {
+        return Math.min(...array);
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!Array.isArray(array) || array.length == 0) {
+        return 0;
+    } else {
+        return Math.max(...array);
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!Array.isArray(array) || array.length == 0) {
+        return 0;
+    } else {
+        return array.reduce((acc, cur) => acc + cur) / array.length;
+    }
+};
